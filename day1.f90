559e2908b6b2ac2e6462371bfd69c63d661d661d
Braden <bradenoptimus@gmail.com>
2023-12-14 20:59:57 -0700

    now it actually loops through a file

--- day1.f90
+++ day1.f90
@@ -3,13 +3,17 @@ program hello
 
     integer :: count = 0
     integer :: holder = 0
-    character :: let = ' '
+    integer :: ios = 0
+    character(len=200) :: buff
 
-    open (unit=10, file='day1.txt', status='old', action='read')
+    open(unit=10, file="day1.txt", status="old")
 
-    do while (let .ne. ' ')
-        print *, let
+    do while (ios .eq. 0)
+        read(10, *, IOSTAT = ios) buff
+        write(*,*) buff
     end do
+
+    close(10)
 
     print *, "Final tally", count, holder
 end program hello
