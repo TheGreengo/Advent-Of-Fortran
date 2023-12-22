026d7b625073210e5eb9aaf3a973f3b43c77ba26
Braden <bradenoptimus@gmail.com>
2023-12-21 19:57:02 -0700

    subroutine made

--- Day 2/day2_pt1.f90
+++ Day 2/day2_pt1.f90
@@ -24,4 +24,25 @@ program main
     ! if they aren't, we set that index of ids to 0
     ! we return the sum of ids
     print *, sum(ids)
+
+    contains 
+
+    logical function GetValid(num, str) result(thing)
+        integer, intent(in) :: num
+        character, intent(in) :: str(num)
+
+        integer :: MaxRed = 12
+        integer :: MaxGreen = 13
+        integer :: MaxBlue = 14
+
+        do i = 1,num
+            print *, MaxRed
+            print *, MaxGreen
+            print *, MaxBlue
+            print *, I
+        end do
+
+        print *, str
+        thing = .false.
+    end function GetValid
 end program main
