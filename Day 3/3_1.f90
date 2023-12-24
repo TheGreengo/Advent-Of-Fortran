b2316e1142b478f6e62d71ddb39d198c07603753
Braden <bradenoptimus@gmail.com>
2023-12-23 18:29:23 -0800

    alfskdjsa

--- Day 3/3_1.f90
+++ Day 3/3_1.f90
@@ -4,24 +4,37 @@ program main
     integer, parameter :: SIZE = 140
     integer :: IOS = 0
     integer :: i = 0
+    integer :: l = 0
+    integer :: k = 0
+    integer :: curr = 5
     character(len=200) :: buff
     character :: mat(SIZE, SIZE)
-    integer :: places(5000, 3)
+    integer :: places(5000, 3) ! row, column, size
+    integer :: vals(5000) 
 
+    vals(:) = 0
     places(:, 1) = -1 
     places(:, 2) = -1 
     places(:, 3) = -1 
+
     open(unit=10, file="day3.txt", status="old")
 
-    do i = 1,140
+    do i = 1, SIZE
         read(10,'(A)', IOSTAT=IOS) buff
-        call LoadMatrix(i, trim(buff))
+        call LoadMatrixRow(i, trim(buff))
     end do
 
+    ! here we go through the matrix and load in all the places and sizes into
+    ! the places matrix
+    call LoadNums
+    ! 
+    call LoadVals
+
+    print *, sum(vals)
 
     contains
 
-    subroutine LoadMatrix(num, str)
+    subroutine LoadMatrixRow(num, str)
         integer, intent(in) :: num
         character, intent(in) :: str(SIZE)
         integer :: j
@@ -31,19 +44,32 @@ program main
         do j = 1,SIZE
             mat(num, j) = str(j)
         end do
-    end subroutine LoadMatrix 
+    end subroutine LoadMatrixRow 
 
     subroutine LoadNums
+        do i = 1, SIZE
+            do k = 1, SIZE
+                print *, mat(i,k)
+            end do
+        end do
     end subroutine LoadNums
 
-    logical function GetValid(x, y, size) result(thang)
+    subroutine LoadVals
+        do while (curr .ne. 0)
+            do k = 1, SIZE
+                print *, mat(i,k)
+            end do
+        end do
+    end subroutine LoadVals
+
+    logical function GetValid(x, y, val) result(thang)
         integer, intent(in) :: x
         integer, intent(in) :: y
-        integer, intent(in) :: size
+        integer, intent(in) :: val
         
         thang = .false.
 
-        print *, x, y, size
+        print *, x, y, val
     end function
 
 end program main
